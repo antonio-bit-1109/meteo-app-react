78317827fc38ba18953e256356940d692fd0b6be
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-12 10:17:08 +0100

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MyNavBarComponent from "./components/MyNavBarComponent";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route></Route>
+                    <Route path="/" element={<MyNavBarComponent />} />
                 </Routes>
             </BrowserRouter>
         </div>
