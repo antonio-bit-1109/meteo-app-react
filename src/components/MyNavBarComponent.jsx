d7ea0c7b824295e1043e52dd82bcbbcc2cdc8d79
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-12 10:46:28 +0100

    gg

--- src/components/MyNavBarComponent.jsx
+++ src/components/MyNavBarComponent.jsx
@@ -3,8 +3,11 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link, useLocation } from "react-router-dom";
 
 const MyNavBarComponent = () => {
+    const location = useLocation();
+
     return (
         <>
             <Navbar expand="lg" className="bg-body-tertiary">
@@ -13,7 +16,10 @@ const MyNavBarComponent = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Link style={{ color: location.pathname === "/" && "red" }} className="nav-link" to={"/"}>
+                                HomePage
+                            </Link>
+
                             <Nav.Link href="#link">Link</Nav.Link>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
